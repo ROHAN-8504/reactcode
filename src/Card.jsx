c51e7a824f3aae493113795f593be8d539e7f4c0
Rohan85041 <chrohankumar85041@gmail.com>
2026-04-24 14:44:00 +0530

    latest chamges

--- src/Card.jsx
+++ src/Card.jsx
@@ -3,9 +3,12 @@ function Card({name,rating,image}) {
   return (
     <>
     <div style={{border:"1px solid black",textAlign:"center" ,maxHeight:"300px", maxWidth:"300px" }}>
+        <div style={{display:"flex"}}>
+
         <img height={50}  width={50}   src={image} alt="" />
         <p>name:{name}</p>
         <p>rating:{rating}</p>
+        </div>
     </div>
     </>
   )
