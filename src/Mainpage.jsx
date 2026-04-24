31d1effec405424e72d6611f1f1e1d996d8d0ff6
Rohan85041 <chrohankumar85041@gmail.com>
2026-04-24 12:20:40 +0530

    react code

---
+++ src/Mainpage.jsx
@@ -0,0 +1,20 @@
+
+function Mainpage() {
+    let name="rohan";
+    let age=100;
+    let comapany="trueminds"
+  return (
+   <>
+   <div style={{border:"solid red 1px"}}>
+
+   <p>name:{name}</p>
+   <p>age:{age}</p>
+   <p>comapany:{comapany}</p>
+   <p>experience:{1+1+1+"years"}</p>
+   </div>
+   </>
+
+  )
+}
+
+export default Mainpage
---
+++ src/prac.js
@@ -0,0 +1,5 @@
+let props={
+    name:"kgf",
+    rating:"5",
+    "image":"https://image.com"
+}
